1cc7f64a073bd56820a449aaf9c5fce7fe5be03a
Digvijay <digv22@gmail.com>
2018-09-22 12:54:13 +0530

    Few things

--- api.jsx
+++ api.jsx
@@ -4,9 +4,6 @@ class Api extends Component {
 
     static getQuerySperator = "/";  //Set Default Sperator ? or / etc
 
-    static baseAddress = window.location.href.indexOf("localhost") > -1 ?
-        "http://localhost:60001/StoreFront/" : "https://api.2paws.xyz/StoreFront/";  //Default API address
-
     constructor() {
         super();
     }
@@ -22,9 +19,9 @@ class Api extends Component {
                 paramsString += this.getQuerySperator + element;
             });
         }
-        console.log(this.baseAddress + endpoint + paramsString);
-        let response = await fetch(this.baseAddress + endpoint + paramsString);
-        return await response.json();
+        console.log( endpoint + paramsString);
+        let response = await fetch( endpoint + paramsString);
+        return await response   ;
     }
 
     /*
@@ -33,12 +30,12 @@ class Api extends Component {
     */
     static async post(endpoint, _body) {
         console.log(_body);
-        let response = await fetch(this.baseAddress + endpoint, {
+        let response = await fetch(endpoint, {
             method: 'post',
             headers: {
                 'Content-Type': 'application/json'
               },
-            body: JSON.stringify(_body)
+            body: JSON.stringify(_body),
         });
         return await response.json();
     }
